1a26c5082aadd3748047c2322f75a39f14d5cc94
Daksh024 <sounkk024@gmail.com>
2020-10-27 00:43:31 +0530

    Sidebar Working!!!

--- admin-panel/src/Components/Sidebar/Sidebar.js
+++ admin-panel/src/Components/Sidebar/Sidebar.js
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
-import "./Sidebar.css"
+import './Sidebar.css'
+// import "./control"
 
 class Sidebar extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.toggle = this.toggle.bind(this);
+    }
+
+    toggle(){
+    }
+
     render() {
+        window.onload = () =>{
+            document.getElementsByClassName('toggle').onclick = () => {
+                document.getElementsByClassName('aside').toggleClass('close');
+            }
+        }
+
+        window.onmouseup = (e) =>{
+            var container = document.getElementsByClassName('aside');
+            if(!container.is(e.target) && container.has(e.target).length === 0)
+            {
+                document.getElementsByClassName('aside').removeClass("close")
+            }
+        }
         return (
-            <div className="sidenav">
-                <a href=".">Company Name</a>
-                <a href="#home">Home</a>
-                <a href="#product">Products</a>
-                <a href="#clients">Clients</a>
-                <a href="#sales">Sales</a>
+            <div className="container">
+                <div className="aside">
+                  <button className="toggle">|||</button>
+                  <h1>Menu</h1>
+                  <div className="nav">
+                    <a href=".">Home</a>
+                    <a href="localhost:3000/#">About Us</a>
+                    <a href=".">Services</a>
+                    <a href=".">Portfolio</a>
+                    <a href=".">Contact Us</a>
+                  </div>
+                </div>
             </div>
         );
     }
